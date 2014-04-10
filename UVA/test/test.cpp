28425ab0d3d65754e29524c535fdf4f8f625e68b
Sergio <sergio.almendros.diaz@gmail.com>
2014-04-10 14:17:24 +0200

    half of the first class

---
+++ UVA/test/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+
+int main(){
+  
+  int i;
+  float f;
+  long long ll; //Mas grande que un int
+  double d;
+  long double ld;
+  string s;
+  char c;
+  bool b;
+  
+  cin >>i>>f>>ll>>d>>ld>>s>>c>>b;
+  cout<<i<<" "<<f<<" "<<ll<<" "<<d<<" "<<ld<<" "<<s<<" "<<c<<" "<<b<<endl;
+  
+  return 0;  
+  
+}
